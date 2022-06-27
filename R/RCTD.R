8984ed8c5586e418790fb4891ed6c41c98cf028a
Constantin Zackl <constantin.zackl@gmail.com>
2022-06-27 15:14:46 +0200

    added deconvolution method

--- R/RCTD.R
+++ R/RCTD.R
@@ -8,40 +8,46 @@
 #' @param n_cores Number of CPU cores to use for the calculation
 #' @export
 
-deconvolute_rctd <- function(sc_counts, cell_types, n_umi_sc = NULL, spatial_counts, spatial_coords, n_umi_sp = NULL, n_cores = 20 ){
-  if (is.null(sc_counts)){
+deconvolute_rctd <- function(sc_counts, cell_types, n_umi_sc = NULL, spatial_counts, spatial_coords, n_umi_sp = NULL, n_cores = 20) {
+  if (is.null(sc_counts)) {
     stop("Parameter 'sc_counts' is missing or null, but is required.")
   }
 
-  if (is.null(cell_types)){
+  if (is.null(cell_types)) {
     stop("Parameter 'cell_types' is missing or null, but is required.")
   }
 
-  if (is.null(spatial_counts)){
+  if (is.null(spatial_counts)) {
     stop("Parameter 'spatial_counts' is missing or null, but is required.")
   }
 
-  if (is.null(spatial_coords)){
+  if (is.null(spatial_coords)) {
     stop("Parameter 'spatial_coords' is missing or null, but is required.")
   }
-  if (n_cores < 1){
+  if (n_cores < 1) {
     stop("Parameter 'n_cores' needs to be a positive integer")
   }
 
   # create reference
-  reference <- spacexr::Reference(counts = sc_counts,
-                                  cell_types = cell_types,
-                                  nUMI = n_umi_sc)
+  reference <- spacexr::Reference(
+    counts = sc_counts,
+    cell_types = cell_types,
+    nUMI = n_umi_sc
+  )
 
   # create spatial dataset
-  puck <- spacexr::SpatialRNA(coords = spatial_coords,
-                              counts = spatial_counts,
-                              nUMI = n_umi_sp)
+  puck <- spacexr::SpatialRNA(
+    coords = spatial_coords,
+    counts = spatial_counts,
+    nUMI = n_umi_sp
+  )
 
   # create rctd dataset
-  rctd_object <- spacexr::create.RCTD(spatialRNA = puck,
-                                      reference = reference,
-                                      max_cores = parallel::detectCores()-1)
+  rctd_object <- spacexr::create.RCTD(
+    spatialRNA = puck,
+    reference = reference,
+    max_cores = parallel::detectCores() - 1
+  )
 
   # perform deconvolution
   rctd_object <- spacexr::run.RCTD(rctd_object)
---
+++ R/deconvolution_algorithms.R
@@ -0,0 +1,41 @@
+#' List of supported deconvolution methods
+#' @export
+#'
+deconvolution_methods <- c("RCTD" = "rctd")
+
+
+
+
+#' Deconvolution
+#' @param spatial_object A SpatialExperiment
+#' @param single_cell_object A SingleCellExperiment
+#' @param cell_type_col Column name of the single_cell_object where the cell type can be found
+#' @param method Deconvolution Method to use, see deconvolution_methods() for a full list of available methods
+#' @param ... Further parameters passed to the selected deconvolution method
+#' @returns The deconvolution result as a table
+#' @export
+deconvolute <- function(spatial_object, single_cell_object, cell_type_col = "cell_ontology_class", method = NULL, ...) {
+  if (is.null(spatial_object)) {
+    stop("Parameter 'spatial_object' is missing or null, but is required.")
+  }
+  if (is.null(single_cell_object)) {
+    stop("Parameter 'single_cell_object' is missing or null, but is required.")
+  }
+
+  # if got the methods name and not the token
+  if (method %in% names(deconvolution_methods)) {
+    method <- deconvolution_methods[[method]]
+  }
+  method <- tolower(method)
+
+  # TODO Type checks for the spatial and single cell object
+  # General type checks will be performed here, also matrix + annotation handling
+  # Method specific processing steps will be located in the switch statement
+
+  deconv <- switch(method,
+    rctd = {
+      deconvolute_rctd(...)
+    }
+  )
+  return(deconv)
+}
