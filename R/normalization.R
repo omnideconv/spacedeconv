c0eb6ab8fdd70f6e9ae903863d7bf9ece736cef7
Constantin Zackl <constantin.zackl@gmail.com>
2022-07-13 15:56:36 +0200

    added normalization function

--- R/normalization.R
+++ R/normalization.R
@@ -1,4 +1,20 @@
-#' Normalize SingleCellExperiment
-normalize_sc <- function(){
+#' Normalize Expression Object
+#'
+#' @param object SingleCellExperiment
+#' @param method normalization method, ("cpm")
+#' @export
+normalize <- function(object, method = "cpm") {
+  if (is.null(object)) {
+    stop("Parameter 'object' is null or missing, but is required!")
+  }
 
+  if (class(object)[[1]] %in% c("SingleCellExperiment", "SpatialExperiment")) {
+    if (method == "cpm") {
+      SingleCellExperiment::cpm(object) <- edgeR::cpm(object)
+    }
+  } else {
+    message("normalization currently only implemented for SingleCellExperiment and SpatialExperiment")
+  }
+
+  return(object)
 }
