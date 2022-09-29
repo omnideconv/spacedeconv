9508f9a589226b6c137ffeb3c78ff5a2d5178026
Constantin Zackl <constantin.zackl@gmail.com>
2022-09-29 13:22:32 +0200

    updated tests

--- tests/testthat/test-model_and_deconvolute.R
+++ tests/testthat/test-model_and_deconvolute.R
@@ -12,7 +12,7 @@ test_that("RCTD Model and deconvolution in one step works", {
 
   expect_equal(
     info = "deconvolution results for all cell types in single cell reference available",
-    obj = colnames(deconv), expected = unique(single_cell_data_2$celltype_major)
+    obj = sort(colnames(deconv)), expected = sort(unique(single_cell_data_2$celltype_major))
   )
 })
 
@@ -46,62 +46,14 @@ test_that("SPOTlight model and deconvolution in one step works", {
 #   )
 # })
 
-test_that("SPOTlight model and deconvolution in one step works", {
-  deconv <- SpaceDeconv::build_and_deconvolute(
-    single_cell_obj = single_cell_data_2,
-    spatial_obj = spatial_data_2,
-    method = "dwls",
-    cell_type_col = "celltype_major",
-    return_object = FALSE,
-    dwls_method="mast_optimized"
-  )
-
-  expect_equal(
-    info = "deconvolution results for all cell types in single cell reference available",
-    obj = sort(colnames(deconv)), expected = sort(unique(single_cell_data_2$celltype_major))
-  )
-})
-
-data("single_cell_data_2")
-data("spatial_data_2")
-
-test_that("RCTD Model and deconvolution in one step works", {
-  deconv <- SpaceDeconv::build_and_deconvolute(
-    single_cell_obj = single_cell_data_2,
-    spatial_obj = spatial_data_2,
-    method = "rctd",
-    cell_type_col = "celltype_major",
-    return_object = FALSE
-  )
-
-  expect_equal(
-    info = "deconvolution results for all cell types in single cell reference available",
-    obj = colnames(deconv), expected = unique(single_cell_data_2$celltype_major)
-  )
-})
-
-test_that("SPOTlight model and deconvolution in one step works", {
-  deconv <- SpaceDeconv::build_and_deconvolute(
-    single_cell_obj = single_cell_data_2,
-    spatial_obj = spatial_data_2,
-    method = "spotlight",
-    cell_type_col = "celltype_major",
-    return_object = FALSE
-  )
-
-  expect_equal(
-    info = "deconvolution results for all cell types in single cell reference available",
-    obj = sort(colnames(deconv)), expected = sort(unique(single_cell_data_2$celltype_major))
-  )
-})
-
-# test_that("CARD model and deconvolution in one step works", {
+# test_that("DWLS model and deconvolution in one step works", {
 #   deconv <- SpaceDeconv::build_and_deconvolute(
 #     single_cell_obj = single_cell_data_2,
 #     spatial_obj = spatial_data_2,
-#     method = "card",
+#     method = "dwls",
 #     cell_type_col = "celltype_major",
-#     return_object = FALSE
+#     return_object = FALSE,
+#     dwls_method="mast_optimized"
 #   )
 #
 #   expect_equal(
@@ -109,24 +61,6 @@ test_that("SPOTlight model and deconvolution in one step works", {
 #     obj = sort(colnames(deconv)), expected = sort(unique(single_cell_data_2$celltype_major))
 #   )
 # })
-
-
-# currently the only omnideconv method
-test_that("DWLS model and deconvolution in one step works", {
-  deconv <- SpaceDeconv::build_and_deconvolute(
-    single_cell_obj = single_cell_data_2,
-    spatial_obj = spatial_data_2,
-    method = "dwls",
-    cell_type_col = "celltype_major",
-    return_object = FALSE,
-    dwls_method="mast_optimized"
-  )
-
-  expect_equal(
-    info = "deconvolution results for all cell types in single cell reference available",
-    obj = sort(colnames(deconv)), expected = sort(unique(single_cell_data_2$celltype_major))
-  )
-})
 
 # currently the only immunedeconv method
 test_that("quanTIseq model and deconvolution in one step works", {
@@ -139,10 +73,9 @@ test_that("quanTIseq model and deconvolution in one step works", {
     dwls_method="mast_optimized"
   )
 
-  expect_equal(
-    info = "deconvolution results for all cell types in single cell reference available",
-    obj = sort(colnames(deconv)), expected = sort(unique(single_cell_data_2$celltype_major))
-  )
+  expect_gt(object = dim(deconv)[1], expected = 1)
+
+  expect_gt(object = dim(deconv)[2], expected = 1)
 })
 
 
--- tests/testthat/test-python.R
+++ tests/testthat/test-python.R
@@ -1,4 +1,4 @@
-test_that("Python environment creation works", {
-  init_python()
-  expect_identical(info = "Python available", object = reticulate::py_available(), expected = TRUE)
-})
+# test_that("Python environment creation works", {
+#   init_python()
+#   expect_identical(info = "Python available", object = reticulate::py_available(), expected = TRUE)
+# })
