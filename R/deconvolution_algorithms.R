183c71d369349cfc5a4186dd9a1f27394dc87bed
Constantin Zackl <constantin.zackl@gmail.com>
2022-10-31 13:09:11 +0100

    updated DESCRIPTION

--- R/deconvolution_algorithms.R
+++ R/deconvolution_algorithms.R
@@ -85,10 +85,9 @@ deconvolution_methods <- c(
 #'
 #' signature <- spacedeconv::build_model(
 #'   single_cell_data_2,
-#'   method = "dwls",
+#'   method = "autogenes",
 #'   cell_type_col = "celltype_major",
-#'   assay_sc = "cpm",
-#'   dwls_method = "mast_optimized"
+#'   assay_sc = "cpm"
 #' )
 build_model <- function(single_cell_obj, cell_type_col = "cell_ontology_class", method = NULL, verbose = FALSE, spatial_obj = NULL, batch_id_col = NULL, assay_sc = "counts", assay_sp = "counts", ...) {
   if (is.null(single_cell_obj)) {
@@ -252,27 +251,13 @@ build_model <- function(single_cell_obj, cell_type_col = "cell_ontology_class", 
 #'
 #' @examples
 #' # more examples can be found in the documentation website
-#' data("single_cell_data_2")
 #' data("spatial_data_2")
 #'
-#' single_cell_data_2 <- spacedeconv::normalize(single_cell_data_2, method = "cpm")
 #' spatial_data_2 <- spacedeconv::normalize(spatial_data_2, method = "cpm")
-#'
-#' signature <- spacedeconv::build_model(
-#'   single_cell_data_2,
-#'   method = "dwls",
-#'   cell_type_col = "celltype_major",
-#'   assay_sc = "cpm",
-#'   dwls_method = "mast_optimized",
-#'   ncores = 12
-#' )
 #'
 #' deconvolution <- spacedeconv::deconvolute(
 #'   spatial_obj = spatial_data_2,
-#'   signature = signature,
-#'   method = "dwls",
-#'   single_cell_obj = single_cell_data_2,
-#'   cell_type_col = "celltype_major"
+#'   method = "estimate",
 #' )
 deconvolute <- function(spatial_obj, signature = NULL, single_cell_obj = NULL, cell_type_col = "cell_ontology_class", method = NULL, batch_id_col = NULL, assay_sc = "counts", assay_sp = "counts", return_object = TRUE, verbose = FALSE, ...) {
   if (is.null(spatial_obj)) {
