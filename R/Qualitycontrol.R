2bd8b60426e5a3718e9707d48dbbb8aad0d25f0f
Constantin Zackl <constantin.zackl@gmail.com>
2023-01-11 13:32:39 +0100

    updated quality control

--- R/Qualitycontrol.R
+++ R/Qualitycontrol.R
@@ -1,34 +1,33 @@
 ## Quality control function
 
-qualitycontrol <- function(spe){
+qualitycontrol <- function(spe) {
+  # Add QC metrics
+  spe <- addPerCellQC(spe)
 
-# Add QC metrics
-spe <- addPerCellQC(spe)
+  # number of total spots
+  ncol(counts(spe))
 
-# number of total spots
-ncol(counts(spe))
+  # total number of genes
+  dim(spe)[1]
 
-# total number of genes
-dim(spe)[1]
+  # Range of total UMIs per spot
+  range(colData(spe)$sum)
 
-#Range of total UMIs per spot
-range(colData(spe)$sum)
+  # % UMIs >= 500
+  sum(colData(spe)$sum >= 500) / dim(spe)[1]
 
-# % UMIs >= 500
-sum(colData(spe)$sum >= 500)/dim(spe)[1]
+  # % UMIs < 500
+  sum(colData(spe)$sum < 500) / dim(spe)[1]
 
-# % UMIs < 500
-sum(colData(spe)$sum < 500)/dim(spe)[1]
+  # Range of detected genes per spot
+  range(colSums(counts(spe) > 0))
 
-# Range of detected genes per spot
-range(colSums(counts(spe)>0))
+  # Plot UMI
+  plot_umi_count(spe, offset_rotation = T)
 
-# Plot UMI
-plot_umi_count(spe, offset_rotation = T)
+  # Plot number of detected genes
+  plot_ndetected_genes(spe, offset_rotation = T)
 
-# Plot number of detected genes
-plot_ndetected_genes(spe, offset_rotation = T)
-
-# Render html output
-rmarkdown::render('./Qualitycontrol.rmd', params = list(output_file = report.html))
+  # Render html output
+  rmarkdown::render(system.file("Qualitycontrol.rmd", package = "spacedeconv"), params = list(output_file = report.html))
 }
