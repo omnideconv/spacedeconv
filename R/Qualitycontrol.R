6507d1cebca06beacb10b0c28d6c57b6ca101406
FranziChiara <109510104+FranziChiara@users.noreply.github.com>
2023-01-13 15:56:42 +0100

    Update Qualitycontrol.R

--- R/Qualitycontrol.R
+++ R/Qualitycontrol.R
@@ -29,6 +29,5 @@ qualitycontrol <- function(spe) {
   plot_ndetected_genes(spe, offset_rotation = T)
 
   # Render html output
-  rmarkdown::render('./Qualitycontrol.rmd', params = list(output_file = report.html))
-  #rmarkdown::render(system.file("Qualitycontrol.rmd", package = "spacedeconv"), params = list(output_file = report.html))
+  rmarkdown::render(system.file("Qualitycontrol.rmd", package = "spacedeconv"), params = list(output_file = report.html))
 }
