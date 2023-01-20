991c49c1fc220d149472567ee3a7d75f0a3472a4
Constantin Zackl <constantin.zackl@gmail.com>
2023-01-20 18:18:10 +0100

    added metacell plots

---
+++ R/metacell_plots.R
@@ -0,0 +1,17 @@
+#' Plot the number of genes per metacells
+#'
+#' @param metacell SingleCellExperiment containing metacells
+#'
+#' @export
+plot_metacell_nCells <- function (metacell){
+  plot(density(colData(metacell)$grouped), main="Number of Cells per metacell")
+}
+
+#' Plot metacell abundance score
+#'
+#' @param metacell SingleCellExperiment containing metacells
+#'
+#' @export
+plot_metacell_abundance <- function (metacell){
+  plot(density(colData(metacell)$percentage), main="Abundance Score per metacell")
+}
