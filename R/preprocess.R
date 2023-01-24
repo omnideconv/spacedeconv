2a3e156968c5158fd01bb3fdc26ca055ce32cb83
Constantin Zackl <constantin.zackl@gmail.com>
2023-01-24 14:05:04 +0100

    updated preprocessing

--- R/preprocess.R
+++ R/preprocess.R
@@ -6,7 +6,7 @@
 #' @param assay assay to use for calculation, you can use any assay but counts is recommended
 #'
 #' @export
-preprocess <- function(object, min_umi = 500, max_umi = 10000, assay = "counts") {
+preprocess <- function(object, min_umi = 500, max_umi = NULL, assay = "counts") {
   cli::cli_rule(left = "spacedeconv")
   cli::cli_progress_step("testing parameter", msg_done = "parameter OK")
 
@@ -29,14 +29,17 @@ preprocess <- function(object, min_umi = 500, max_umi = 10000, assay = "counts")
 
   object <- object[, colSums(SummarizedExperiment::assay(object, assay)) >= min_umi]
 
-  # max UMI Count per Observation
-  nObservation <- sum(colSums(SummarizedExperiment::assay(object, assay)) > max_umi)
-  cli::cli_progress_step(
-    msg = paste0("Removing ", nObservation, " observations with umi count abvove threshold"),
-    msg_done = paste0("Removed ", nObservation, " observations with umi count above threshold")
-  )
 
-  object <- object[, colSums(SummarizedExperiment::assay(object, assay)) <= max_umi]
+  # max UMI Count per Observation, only if value is provided
+  if (!is.null(max_umi)){
+    nObservation <- sum(colSums(SummarizedExperiment::assay(object, assay)) > max_umi)
+    cli::cli_progress_step(
+      msg = paste0("Removing ", nObservation, " observations with umi count abvove threshold"),
+      msg_done = paste0("Removed ", nObservation, " observations with umi count above threshold")
+    )
+
+    object <- object[, colSums(SummarizedExperiment::assay(object, assay)) <= max_umi]
+  }
 
   # remove all zero genes
   nVariable <- sum(rowSums(SummarizedExperiment::assay(object, assay)) == 0)
