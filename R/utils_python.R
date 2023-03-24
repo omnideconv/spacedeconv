2b05444b193138bc397f2454218a330fb4fe87d1
Constantin Zackl <constantin.zackl@gmail.com>
2023-03-24 15:52:36 +0100

    updates

--- R/utils_python.R
+++ R/utils_python.R
@@ -2,47 +2,47 @@
 #'
 #' @param python (optional) If own python should be used please indicate it's binaries
 #'
-init_python <- function(python = NULL) {
-  if (!reticulate::py_available()) {
-    if (is.null(python)) {
-      if (!dir.exists(reticulate::miniconda_path())) {
-        message("Setting python version in miniconda to be 3.8")
-        Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.8)
-        message("Setting up miniconda environment..")
-        suppressMessages(reticulate::install_miniconda())
-      }
-      check_env()
-      reticulate::use_miniconda(condaenv = "spacedeconv", required = TRUE)
-      config <- reticulate::py_config()
-      if (!python_available()) {
-        message("Python not available")
-        print(config)
-        message(
-          "Please indicate your version of python calling init_python(python=your/python)"
-        )
-      }
-    } else {
-      reticulate::use_python(python = python)
-      reticulate::py_config()
-    }
-  }
-}
+# init_python <- function(python = NULL) {
+#   if (!reticulate::py_available()) {
+#     if (is.null(python)) {
+#       if (!dir.exists(reticulate::miniconda_path())) {
+#         message("Setting python version in miniconda to be 3.8")
+#         Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.8)
+#         message("Setting up miniconda environment..")
+#         suppressMessages(reticulate::install_miniconda())
+#       }
+#       check_env()
+#       reticulate::use_miniconda(condaenv = "spacedeconv", required = TRUE)
+#       config <- reticulate::py_config()
+#       if (!python_available()) {
+#         message("Python not available")
+#         print(config)
+#         message(
+#           "Please indicate your version of python calling init_python(python=your/python)"
+#         )
+#       }
+#     } else {
+#       reticulate::use_python(python = python)
+#       reticulate::py_config()
+#     }
+#   }
+# }
 
-check_env <- function() {
-  if (!"spacedeconv" %in% reticulate::conda_list()$name) {
-    reticulate::conda_create(envname = "spacedeconv", packages = c("pip", "numpy"))
-    cli::cli_alert_info("created conda environment 'spacedeconv'")
-  }
-}
+# check_env <- function() {
+#   if (!"spacedeconv" %in% reticulate::conda_list()$name) {
+#     reticulate::conda_create(envname = "spacedeconv", packages = c("pip", "numpy"))
+#     cli::cli_alert_info("created conda environment 'spacedeconv'")
+#   }
+# }
 
 
 #' Checks if python is available in environment
 #'
 #' @return boolean
 #'
-python_available <- function() {
-  return(reticulate::py_available())
-}
+# python_available <- function() {
+#   return(reticulate::py_available())
+# }
 
 #' Checks if anndata package is loaded
 #'
