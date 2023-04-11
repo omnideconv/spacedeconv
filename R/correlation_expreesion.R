395e046adc3dbef6c0f885c01561451815a0943e
FranziChiara <109510104+FranziChiara@users.noreply.github.com>
2023-04-11 11:15:32 +0200

    Update correlation_expreesion.R
    small adaptions

--- R/correlation_expreesion.R
+++ R/correlation_expreesion.R
@@ -18,7 +18,7 @@ corr_expr <- function(sig, log = FALSE, cor_method = c("pearson", "spearman"), m
   }
   corr_matrix <- corr.test(sig, method = cor_method, adjust = "none")$r
   corr_p <- corr.test(sig, adjust = "none")$p
-  corrplot(correlation, p.mat = corr_p, method = 'color', diag = FALSE, type = 'lower',
+  corrplot(corr_matrix, p.mat = corr_p, method = 'color', diag = FALSE, type = 'lower',
            sig.level = c(0.001, 0.01, 0.05), pch.cex = 0.9,
            insig = 'label_sig', pch.col = 'black', tl.col = "black",
            title = "Correlation expression with significance",
@@ -26,7 +26,7 @@ corr_expr <- function(sig, log = FALSE, cor_method = c("pearson", "spearman"), m
   cat(paste0("\t", c("Significance correlation:", "* <0.05", "** <0.01", "*** <0.001"), "\n"))
   if (matrix == TRUE) {
     print("Correlation of expression")
-    print(correlation)
+    print(corr_matrix)
     print("p_correlation")
     print(corr_p)
   }
