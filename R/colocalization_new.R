eff365414a6f157419d904e3f1aa450ebba92b0d
FranziChiara <109510104+FranziChiara@users.noreply.github.com>
2023-04-25 14:15:26 +0200

    First steps

--- R/colocalization_new.R
+++ R/colocalization_new.R
@@ -8,6 +8,7 @@
 #' @param density logical
 #' @param niter permutations
 #' @param presence_matrix presence matrix if available
+#' @param threshold threshold for custom presence determination
 #' @returns statistics and graph
 #' @export
 cell_pair_localization <- function(spe, method = NULL, distance = 0,
@@ -18,41 +19,74 @@ cell_pair_localization <- function(spe, method = NULL, distance = 0,
   }
 
   ########## cell type test
-  # Calculate presence matrix based on abtimode cutoff approach
+  # Calculate presence matrix based on antimode cutoff approach
   if (is.null(presence_matrix)) {
     presence_matrix <- presence(spe, method)
   }
   # Set NA values to absent
   presence_matrix[is.na(presence_matrix)] <- FALSE
 
-  # create presence/absence vector for both celltypes
+  if (distance == 0) {
+    # create presence/absence vector for both celltypes
 
-  A <- presence_matrix[, cell_type_A]
-  B <- presence_matrix[, cell_type_B]
+    A <- presence_matrix[, cell_type_A]
+    B <- presence_matrix[, cell_type_B]
+
+
+  }else if (distance > 0) {
+    df <- as.data.frame(colData(spe))
+
+    # # for all spots get the spots in distance and calculate mean value
+    iniche <- vector(mode = "list", length = niter)
+    for (spot in rownames(df)) {
+      iniche[spot] <- list(get_iniche(df, get_spot_coordinates(df, spot), distance = distance))
+    }
+
+    # determine presence/absence of celltypes in the iniches
+    niche_pres_A <- rep(FALSE, length(iniche))
+    niche_pres_B <- rep(FALSE, length(iniche))
+
+    for (i in 1:length(iniche)) {
+      bar <- iniche[[i]]
+
+      uni <- presence[bar, cell_type_A]
+      dui <- presence[bar, cell_type_B]
+
+      if (sum(uni) >= 1) {
+        niche_pres_A[i] <- TRUE
+      }
+
+      if (sum(dui) >= 1) {
+        niche_pres_B[i] <- TRUE
+      }
+    }
+
+    # combine
+    niche_A_B <- rbind(niche_pres_A, niche_pres_B)
+    rownames(niche_A_B) <- c(cell_type_A, cell_type_B)
+
+    A <- niche_A_B[cell_type_A, ]
+    B <- niche_A_B[cell_type_B, ]
+  }
 
   # Calculate real colocalization and avoidance events
   loc_original <- coloc_avoid(A, B)
   coloc <- loc_original["coloc"]
   avoid <- loc_original["avoid"]
 
+
+
+  ################  Randomization??
   # Create randomized presence matrices and calculate corresponding colocalization/avoidance events
   coloc_rand <- vector(length = niter)
   avoid_rand <- vector(length = niter)
 
   for (i in 1:niter) {
-    # randomize coordinates
-    # Shuffle barcode names together with spatial coordinates
-    shuffle_spe <- colData(spe)[sample(nrow(colData(spe))), c("in_tissue", "array_row", "array_col", "sample_id")]
-    # Create new colData with shuffeled coordinates
-    new_col <- cbind(shuffle_spe, colData(spe)[, -c(1:4)]) ## How to remove the first 4 columns by name?
-    colData(spe) <- new_col
-
-    # Calculate presence matrix for randomized version
-    presence_matrix_rand <- presence(spe, method)
-
-    # Create randomized presence/absence vector for both cell types
-    A_rand <- presence_matrix_rand[, cell_type_A]
-    B_rand <- presence_matrix_rand[, cell_type_B]
+    # shuffle coordinates of presence/absence vectors --> names need to be in the same order for coloc_avoid function
+    A_rand <- sample(A)
+    names(A_rand) <- names(A)
+    B_rand <- sample(B)
+    names(A_rand) <- names(B)
 
     # Calculate randomized colocalization and avoidance events
     loc_rand <- coloc_avoid(A_rand, B_rand)
@@ -60,22 +94,23 @@ cell_pair_localization <- function(spe, method = NULL, distance = 0,
     avoid_rand[i] <- loc_rand["avoid"]
   }
 
+  ################# add density graph and statistics
   if (density) {
     dens <- density(coloc_rand)
     p <- plot(dens,
-      main = paste0(
-        "Colocalization ",
-        cell_type_A, "_", cell_type_B
-      ),
-      xlim = range(coloc, coloc_rand)
+              main = paste0(
+                "Colocalization ",
+                cell_type_A, "_", cell_type_B
+              ),
+              xlim = range(coloc, coloc_rand)
     )
     abline(v = coloc, col = "red")
     plot(density(avoid_rand),
-      main = paste0(
-        "Avoidance ",
-        cell_type_A, "_", cell_type_B
-      ),
-      xlim = range(avoid, avoid_rand)
+         main = paste0(
+           "Avoidance ",
+           cell_type_A, "_", cell_type_B
+         ),
+         xlim = range(avoid, avoid_rand)
     )
     abline(v = avoid, col = "red")
   }
@@ -109,44 +144,3 @@ cell_pair_localization <- function(spe, method = NULL, distance = 0,
 
 
 
-
-##### Add distance parameter
-##
-if (distance == 0) {
-
-} else if (distance > 0) {
-  df <- as.data.frame(colData(spe))
-
-  # # for all spots get the spots in distance and calculate mean value
-  iniche <- vector(mode = "list", length = niter)
-  for (spot in rownames(df)) {
-    iniche[spot] <- list(get_iniche(df, get_spot_coordinates(df, spot), distance = distance))
-  }
-
-  # determine presence/absence of celltypes in the iniches
-  niche_pres_A <- rep(FALSE, length(iniche))
-  niche_pres_B <- rep(FALSE, length(iniche))
-
-  for (i in 1:length(iniche)) {
-    bar <- iniche[[i]]
-
-    uni <- presence[bar, cell_type_A]
-    dui <- presence[bar, cell_type_B]
-
-    if (sum(uni) >= 1) {
-      niche_pres_A[i] <- TRUE
-    }
-
-    if (sum(dui) >= 1) {
-      niche_pres_B[i] <- TRUE
-    }
-  }
-
-  # combine
-  niche_A_B <- rbind(niche_pres_A, niche_pres_B)
-  rownames(niche_A_B) <- c(cell_type_A, cell_type_B)
-
-  A <- niche_A_B[cell_type_A, ]
-  B <- niche_A_B[cell_type_B, ]
-}
-##
