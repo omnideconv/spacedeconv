fddd166fad5afcdf34756d0c0833179a6f5b6e1a
Constantin Zackl <constantin.zackl@gmail.com>
2024-02-26 23:46:24 +0100

    updated package startup

--- R/zzz.R
+++ R/zzz.R
@@ -7,62 +7,71 @@ NULL
 .onLoad <- function(libname, pkgname) {
   cli::cli_alert("checking spacedeconv environment and dependencies")
 
-  sink("/dev/null")
 
-  suppressWarnings({
-    tmp <- utils::capture.output({
-      # Make sure miniconda is installed
-      if (!dir.exists(reticulate::miniconda_path())) {
-        message("Setting python version in miniconda to be 3.8")
-        Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.8)
-        message("Setting up miniconda environment..")
-        suppressMessages(reticulate::install_miniconda())
-      }
+  temp_file <- tempfile()
 
+  # Redirect output to the temporary file
+  sink(temp_file)
 
-      # We ensure to have the r-spacedeconv env
-      # if (!file.exists(reticulate::conda_python("r-reticulate"))) {
-      if (!("r-omnideconv" %in% reticulate::conda_list()$name)) {
-        reticulate::conda_create(envname = "r-omnideconv")
-      }
-
-      paths <- reticulate::conda_list()
-      path <- paths[paths$name == "r-omnideconv", 2]
-      if (.Platform$OS.type == "windows") {
-        path <- gsub("\\\\", "/", path)
-      }
-      path.bin <- gsub("/envs/r-omnideconv/python.exe", "/library/bin", path)
-      Sys.setenv(PATH = paste(path.bin, Sys.getenv()["PATH"], sep = ";"))
-      Sys.setenv(RETICULATE_PYTHON = path)
+  suppressMessages({
+    suppressWarnings({
+      tmp <- utils::capture.output({
+        #reticulate::py_capture_output({
+          # Make sure miniconda is installed
+          if (!dir.exists(reticulate::miniconda_path())) {
+            message("Setting python version in miniconda to be 3.8")
+            Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.8)
+            message("Setting up miniconda environment..")
+            suppressMessages(reticulate::install_miniconda())
+          }
 
 
-      reticulate::use_miniconda(condaenv = "r-omnideconv", required = TRUE)
-      reticulate::py_config()
-      reticulate::configure_environment(pkgname, force = TRUE)
+          # We ensure to have the r-spacedeconv env
+          # if (!file.exists(reticulate::conda_python("r-reticulate"))) {
+          if (!("r-omnideconv" %in% reticulate::conda_list()$name)) {
+            reticulate::conda_create(envname = "r-omnideconv")
+          }
 
-      if (!reticulate::py_module_available("anndata")) {
-        anndata::install_anndata()
-      }
+          paths <- reticulate::conda_list()
+          path <- paths[paths$name == "r-omnideconv", 2]
+          if (.Platform$OS.type == "windows") {
+            path <- gsub("\\\\", "/", path)
+          }
+          path.bin <- gsub("/envs/r-omnideconv/python.exe", "/library/bin", path)
+          Sys.setenv(PATH = paste(path.bin, Sys.getenv()["PATH"], sep = ";"))
+          Sys.setenv(RETICULATE_PYTHON = path)
 
-      # if (!reticulate::py_module_available("python.app")) { # only MAC!
-      #   reticulate::py_install("python.app", pip=TRUE)
-      # }
 
-      if (!reticulate::py_module_available("sklearn")) {
-        reticulate::py_install("scikit-learn")
-      }
+          reticulate::use_miniconda(condaenv = "r-omnideconv", required = TRUE)
+          reticulate::py_config()
+          reticulate::configure_environment(pkgname, force = TRUE)
 
-      if (!reticulate::py_module_available("community")) {
-        reticulate::py_install("python-louvain", pip = TRUE)
-      }
-      if (!reticulate::py_module_available("cell2location")) {
-        reticulate::py_install("cell2location", pip = TRUE)
-      }
-    })
+          if (!reticulate::py_module_available("anndata")) {
+            anndata::install_anndata()
+          }
+
+          # if (!reticulate::py_module_available("python.app")) { # only MAC!
+          #   reticulate::py_install("python.app", pip=TRUE)
+          # }
+
+          if (!reticulate::py_module_available("sklearn")) {
+            reticulate::py_install("scikit-learn")
+          }
+
+          if (!reticulate::py_module_available("community")) {
+            reticulate::py_install("python-louvain", pip = TRUE)
+          }
+          if (!reticulate::py_module_available("cell2location")) {
+            reticulate::py_install("cell2location", pip = TRUE)
+          }
+        })
+      })
+    #})
   })
 
   # bug fix
   Csparse_validate <- "CsparseMatrix_validate"
 
-  sink()
+  sink(NULL)
+  unlink(temp_file)
 }
