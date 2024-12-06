bb5972fa77271d7edf1c80ab6f3687e9d8d4038f
Constantin Zackl <constantin.zackl@gmail.com>
2024-12-06 09:39:49 +0100

    added DOT parameter

--- R/DOT.R
+++ R/DOT.R
@@ -15,12 +15,17 @@ build_model_dot <- function() {
 #' @param assay_sc assay of single_cell_obj to use
 #' @param assay_sp assay of spatial_obj to use
 #' @param result_name token to identify deconvolution results in object, default = "dot"
+#' @param ratios_weight This parameter determines how much the cell type ratios in the reference sc dataset are expected to match the ratios in the target spatial data. A higher value would match the cell type ratios more closely. By default this is 0 since we do not assume that the modalities match, but usually a value between 0.1 and 0.25 is reasonable depending on the quality of the sc data. If the two modalities are expected to match perfectly (e.g., come from the same tissue), a value closer to 1 would give the best results.
+#' @param max_spot_size this parameter determines the maximum number of cells per spot and a higher value indicates lower resolution in the spatial data. The results should be robust for the default value but will give better estimates of the absolute abundances if the appropriate parameter is passed. For Visium, this is set to 20 (default). For ST it should be set to a higher value (e.g., 200).
 #' @param ... additional parameters passed to DOTr methods
 deconvolute_dot <- function(single_cell_obj,
                             spatial_obj,
                             cell_type_col = "cell_ontology_class",
                             assay_sc = "counts", assay_sp = "counts",
-                            result_name = "dot", ...) {
+                            result_name = "dot",
+                            ratios_weight = 0,
+                            max_spot_size = 20,
+                            ...) {
   if (is.null(single_cell_obj)) {
     stop("Parameter 'single_cell_obj' is missing or null, but is required!")
   }
@@ -69,7 +74,7 @@ deconvolute_dot <- function(single_cell_obj,
   dot <- create.DOT(dot.srt, dot.ref, ...)
 
   # Run Deconvolution
-  dot <- run.DOT.lowresolution(dot, ...) # additional parameters passed as ...
+  dot <- run.DOT.lowresolution(dot, ratios_weight = ratios_weight, max_spot_size = max_spot_size, ...) # additional parameters passed as ...
 
   # attach token
   deconvolution_result <- attachToken(dot@weights, result_name) # add dot_ to column names
