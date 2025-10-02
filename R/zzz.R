0f4eed6ad1fa8b07c1271d98f493481c6477a893
felixpetschko <felixpetschko@gmail.com>
2025-10-02 16:41:48 +0200

    reduced zzz.R setup

--- R/zzz.R
+++ R/zzz.R
@@ -5,57 +5,26 @@
 NULL
 
 .onLoad <- function(libname, pkgname) {
+
   cli::cli_alert("checking spacedeconv environment and dependencies")
 
   envname <- getOption("omnideconv.conda_env", default = "r-omnideconv")
 
   cli::cli_alert("Using conda environment '{envname}'")
 
-  temp_file <- tempfile()
-  sink(temp_file)
+  if (!dir.exists(reticulate::miniconda_path())) {
+    message("Setting python version in miniconda to be 3.10")
+    Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.10)
+    message("Installing miniconda..")
+    suppressMessages(reticulate::install_miniconda())
+  }
 
-  invisible({
-    suppressMessages({
-      suppressWarnings({
-        # tmp <- reticulate::py_capture_output({
-        tmp2 <- utils::capture.output({
-          # reticulate::py_capture_output({
-          # Make sure miniconda is installed
-          if (!dir.exists(reticulate::miniconda_path())) {
-            message("Setting python version in miniconda to be 3.10")
-            Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.10)
-            message("Setting up miniconda environment..")
-            suppressMessages(reticulate::install_miniconda())
-          }
+  if (!(envname %in% reticulate::conda_list()$name)) {
+    stop(cli::format_error("Conda environment '{envname}' not found."), call. = FALSE)
+  }
 
-
-          # We ensure to have the r-spacedeconv env
-          # if (!file.exists(reticulate::conda_python("r-reticulate"))) {
-          if (!(envname %in% reticulate::conda_list()$name)) {
-            reticulate::conda_create(envname = envname)
-          }
-
-          paths <- reticulate::conda_list()
-          path <- paths[paths$name == envname, 2]
-          if (.Platform$OS.type == "windows") {
-            path <- gsub("\\\\", "/", path)
-          }
-          path.bin <- gsub(paste0("/envs/", envname, "/python.exe"), "/library/bin", path)
-          Sys.setenv(PATH = paste(path.bin, Sys.getenv()["PATH"], sep = ";"))
-          Sys.setenv(RETICULATE_PYTHON = path)
-
-
-          reticulate::use_miniconda(condaenv = envname, required = FALSE)
-          reticulate::py_config()
-          reticulate::configure_environment(pkgname, force = FALSE)
-        })
-      })
-    })
-  })
+  reticulate::use_miniconda(condaenv = envname, required = TRUE)
 
   # bug fix
   Csparse_validate <- "CsparseMatrix_validate"
-
-  sink(NULL)
-  unlink(temp_file)
 }
