6d092a2e0710d81c01ea2f2fdd6f1f29903d3d80
felixpetschko <felixpetschko@gmail.com>
2025-10-02 14:56:40 +0200

    Changed Python version to 3.10

--- R/zzz.R
+++ R/zzz.R
@@ -22,8 +22,8 @@ NULL
           # reticulate::py_capture_output({
           # Make sure miniconda is installed
           if (!dir.exists(reticulate::miniconda_path())) {
-            message("Setting python version in miniconda to be 3.8")
-            Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.8)
+            message("Setting python version in miniconda to be 3.10")
+            Sys.setenv(RETICULATE_MINICONDA_PYTHON_VERSION = 3.10)
             message("Setting up miniconda environment..")
             suppressMessages(reticulate::install_miniconda())
           }
