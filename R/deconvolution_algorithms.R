6da1cfd4113095a7f2a944f08b6ed99a77c33912
felixpetschko <felixpetschko@gmail.com>
2025-10-30 12:15:27 +0100

    Removed bseqsc package remainders

--- R/deconvolution_algorithms.R
+++ R/deconvolution_algorithms.R
@@ -23,7 +23,6 @@ deconvolution_methods <- c(
   "spatialDWLS" = "spatialdwls",
   "cell2location" = "cell2location",
   "DOT" = "dot",
-  "BSeq-sc" = "bseqsc",
   "CIBERSORTx" = "cibersortx",
   "CDSeq" = "cdseq",
   "CPM" = "cpm",
