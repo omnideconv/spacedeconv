b5d055c8bfae8649f1b0d4da8bd0c02dc3eaeb11
felixpetschko <felixpetschko@gmail.com>
2025-10-30 12:13:12 +0100

    Removed bayesprism package remainders

--- R/deconvolution_algorithms.R
+++ R/deconvolution_algorithms.R
@@ -23,7 +23,6 @@ deconvolution_methods <- c(
   "spatialDWLS" = "spatialdwls",
   "cell2location" = "cell2location",
   "DOT" = "dot",
-  "BayesPrism" = "bayesprism",
   "BSeq-sc" = "bseqsc",
   "CIBERSORTx" = "cibersortx",
   "CDSeq" = "cdseq",
